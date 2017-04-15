e2af371a14d559a7097efebc46e60abbe2e80272
cjpennock <cjpennock@gmail.com>
2017-04-15 10:47:14 -0400

    Get, Post, Delete working

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const db = require('sqlite');
+
+let app = express();
+const port = 8008;
+
+const parser = require('body-parser');
+app.use(parser.json())
+
+app.use('/', express.static('./public/', {
+    'index': ['index.html']
+}));
+
+app.get('/get', (req, res, next) => {
+    res.header('Content-Type', 'application/json');
+	res.send({
+        "message": "GET request to the homepage",
+        "success": true
+    })
+})
+
+app.post('/', (req, res, next) => {
+    res.header('Content-Type', 'application/json');
+    res.send({
+        "message": "POST request to the homepage",
+        "success": true
+    })
+})
+
+app.delete('/', (req, res, next) => {
+    res.header('Content-Type', 'application/json');
+    res.send({
+        "message": "DELETE request to the homepage",
+        "success": true
+    });
+});
+
+Promise.resolve()
+    .then(() => db.open('./database.sqlite', { Promise }))
+    .then(() => db.migrate({ force: 'last' }))
+    .then(() => app.listen(port))
+    .then(() => {
+        console.log(`Server started on port ${port}`)
+     })
+    .catch(err => console.error(err.stack))
