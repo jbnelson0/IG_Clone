0a50cfa149bcec328b9d29d743a36fb4fb23aeb6
jbnelson0 <james.bnelson0@gmail.com>
2017-04-19 18:47:08 -0400

    Add files via upload

--- users.js
+++ users.js
@@ -2,13 +2,16 @@ const db = require('sqlite');
 
 const DB = {}
 
-DB.createNewUser = (data) => {
-	return db.run("INSERT INTO users (user_id, first_name, last_name, email) values (?, ?, ?, ?)", [data])
-
+DB.createNewUser = (first_name, last_name, username, password) => {
+	return db.run("INSERT INTO users (first_name, last_name, username, password) values (?, ?, ?, ?)", [first_name, last_name, username, password])
 }
 
 DB.findUserByUserID = (user_id) =>{
-	return db.run("SELECT * FROM users WHERE id = ? ", [user_id])
+	return db.all("SELECT * FROM users WHERE id = ? ", [user_id])
+}
+
+DB.selectAllUsers = (table_name) => {
+	return db.all("SELECT * FROM" + table_name)
 }
 
 module.exports = DB
