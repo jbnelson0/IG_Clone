b5bdd9b9bdb9b7a1c374d97f8fe533f82e66def2
jbnelson0 <james.bnelson0@gmail.com>
2017-04-19 20:24:04 -0400

    merged in master

--- users.js
+++ users.js
@@ -1,19 +1,6 @@
 
 const SQL = require('sqlite');
 
-const db = require('./database.sqlite')
-
-    db.all('SELECT * FROM users')
-        .then((data) => {
-            res.header('Content-Type', 'application/json');
-            res.send({ users: data });
-        })
-        .catch((e) => {
-            res.status(401);
-        });
-
-const db = require('sqlite');
-
 const DB = {}
 
 DB.createNewUser = (first_name, last_name, username, password) => {
