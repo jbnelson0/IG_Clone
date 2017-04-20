409e0e852ff301bd4ed1932d151dca33cb79c304
jbnelson0 <james.bnelson0@gmail.com>
2017-04-19 20:12:31 -0400

    Update users.js

--- users.js
+++ users.js
@@ -4,20 +4,6 @@ const SQL = require('sqlite');
 const DB = {}
 
 DB.createNewUser = (first_name, last_name, username, password) => {
-<<<<<<< HEAD
-    return db.run("INSERT INTO users (first_name, last_name, username, password) values (?, ?, ?, ?)", [first_name, last_name, username, password])
-}
-
-DB.findUserByUserID = (user_id) =>{
-    return db.all("SELECT * FROM users WHERE id = ? ", [user_id])
-}
-
-DB.selectAllUsers = (table_name) => {
-    return db.all("SELECT * FROM" + table_name)
-}
-
-module.exports = DB
-=======
 	return db.run("INSERT INTO users (first_name, last_name, username, password) values (?, ?, ?, ?)", [first_name, last_name, username, password])
 }
 
@@ -31,4 +17,3 @@ DB.selectAllUsers = (table_name) => {
 
 module.exports = DB
 
->>>>>>> 5bbbdf1e4a4987ac52765a891d87a2e7565e51c5
