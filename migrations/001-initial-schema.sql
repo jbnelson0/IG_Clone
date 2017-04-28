ed7fffea9858e4d179fe105665cc0535ac787ad2
jbnelson0 <james.bnelson0@gmail.com>
2017-04-28 15:34:37 -0400

    added posts table for feed and posts.js

--- migrations/001-initial-schema.sql
+++ migrations/001-initial-schema.sql
@@ -4,15 +4,26 @@ CREATE TABLE users(
 	username	TEXT	NOT NULL,
 	password	TEXT	NOT NULL,
 	first_name	TEXT	NOT NULL,
-	last_name	TEXT	NOT NULL
+	last_name	TEXT	NOT NULL,
+	profile_pic	TEXT
 );
 
-INSERT INTO users (username, password, first_name, last_name) values ('jsmith1', 'abc123', 'John', 'Smith')
-INSERT INTO users (username, password, first_name, last_name) values ('aadams1', 'ghi789', 'Amy', 'Adams')
-INSERT INTO users (username, password, first_name, last_name) values ('jsnow1', 'def456', 'John', 'Snow')
+CREATE TABLE posts(
+	id 	INTEGER	PRIMARY KEY,
+	post 	TEXT	NOT NULL
+);
+
+INSERT INTO users (username, password, first_name, last_name) VALUES ('jsmith1', 'abc123', 'John', 'Smith');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('aadams1', 'ghi789', 'Amy', 'Adams');
+INSERT INTO users (username, password, first_name, last_name) VALUES ('jsnow1', 'def456', 'John', 'Snow');
+
+INSERT INTO posts (id, post) VALUES (1, 'https://www.bigstockphoto.com/images/homepage/2016_bigstock_picks.jpg');
+INSERT INTO posts (id, post) VALUES (2, 'https://www.sitebuilderreport.com/assets/facebook-stock-up-446fff24fb11820517c520c4a5a4c032.jpg');
+INSERT INTO posts (id, post) VALUES (3, 'https://www.bigstockphoto.com/images/homepage/2016_bigstock_video.jpg');
+
 
 -- DOWN
 
 
 DROP TABLE users;
-
+DROP TABLE posts;
